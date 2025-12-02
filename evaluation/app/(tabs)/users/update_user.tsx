8f8a8950510fc323809bc252061710382cd7fabf
Javier Rosado <javy@Javiers-MacBook-Air.local>
2025-12-01 21:18:29 -0500

    fixes new hire position when updating a user

--- evaluation/app/(tabs)/users/update_user.tsx
+++ evaluation/app/(tabs)/users/update_user.tsx
@@ -171,15 +171,21 @@ const UpdateUser = () => {
               options={options}
               returnOption
               onSelect={(opt) => {
+                let newHire = "";
+
+                if (
+                  opt.__k === "New Hire W/ Ppe Fab" ||
+                  opt.__k === "New Hire W/ Ppe Kill"
+                ) {
+                  newHire = "NON-QUALIFIED";
+                }
+
                 setAddEmployeeInfo((prev: any) => ({
                   ...prev,
-                  position: opt?.__k ?? opt?.value ?? opt,
+                  position: newHire || (opt?.__k ?? opt?.value ?? opt),
                   department: opt?.children?.department_name ?? "",
                 }));
-                setErrors((prev: any) => ({
-                  ...prev,
-                  position: "",
-                }));
+                setErrors((prev: any) => ({ ...prev, position: "" }));
               }}
               selectedValue={addEmployeeInfo?.position}
               loadData={loadJobOptions}
